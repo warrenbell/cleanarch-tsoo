ed60a633102f7d03efc8e9d3c338441e3d6c171a
warrenbell <warrenbell2@gmail.com>
2018-06-24 19:54:02 -0700

    Commit more name changeing

--- src/infrastructure/src/index.ts
+++ src/infrastructure/src/index.ts
@@ -1,5 +1,5 @@
 // Layer 4 (blue) local dependencies from infrastructure
-// This is were we can wire together different implementations of SaveWidgetEntityGatewayInt
+// This is were we can wire together different implementations of IEntityGateway using the from path
 import { EntityGateway } from "./persistence/anydb/EntityGateway"
 
 export { EntityGateway };
